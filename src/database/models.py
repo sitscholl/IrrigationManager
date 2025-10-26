0a13cc6de1bfef00f4eeac4927da27c5a1df206d
sitscholl <tscholl.simon@gmail.com>
2025-10-26 20:59:58 +0100

    expand db models and start implementding db methods

--- src/database/models.py
+++ src/database/models.py
@@ -13,6 +13,8 @@ class Field(Base):
     name = Column(String, nullable = False, unique = True)
     reference_station = Column(String, nullable = False)
     soil_type = Column(String, nullable = False)
+    area_ha = Column(Float, nullable = False)
+    p_allowable = Column(Float, nullable = False, default = 0)
 
 class Irrigation(Base):
     __tablename__ = 'irrigation_events'
@@ -20,6 +22,7 @@ class Irrigation(Base):
     id = Column(Integer, primary_key = True)
     field_id = Column(str, ForeignKey("fields.id"), nullable = False)
     date = Column(DateTime, nullable = False)
+    method = Column(String, nullable = False)
     amount = Column(Float, default = 100)
 
     field = relationship(Field, back_populates = 'irrigation_events')
