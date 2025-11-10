41d56029f58385869e03b07a51b560525a23fa17
sitscholl <tscholl.simon@gmail.com>
2025-11-10 18:24:42 +0100

    first implementation of PenmanDailyCalculator

---
+++ src/et0/penman_daily.py
@@ -0,0 +1,50 @@
+from pyet import pm_fao56
+import pandas as pd
+
+from typing import TYPE_CHECKING
+
+from .base import ET0Calculator
+
+if TYPE_CHECKING:
+    from ..et_correction import ETCorrection
+    from ..meteo import Station
+
+
+class PenmanDailyCalculator(ET0Calculator):
+
+    def __init__(self, config, corrector: ETCorrection | None = None):
+        self.config = config
+        self.corrector = corrector
+
+    @property
+    def name(self):
+        return "PenmanDaily"
+
+    def _validate_data(self, data):
+
+        if data.index.dtype != 'datetime64[ns]' and pd.infer_freq(data.index) != 'D':
+            raise ValueError(f"Index of input data has to be of type datetime with daily frequency. Got {data.index.dtype} and {pd.infer_freq(data.index)}")
+
+    def calculate(self, station: "Station", correct: bool = True):
+
+        meteo_data = station.data
+        self._validate_data(meteo_data)
+
+        et = pm_fao56(
+            meteo_data.tair_2m, 
+            meteo_data.wind_speed, 
+            rs=meteo_data.solar_radiation,
+            elevation=station.elevation, 
+            lat=station.latitude, 
+            tmax=meteo_data.tmax, 
+            tmin=meteo_data.tmin, 
+            rh=meteo_data.relative_humidity
+            )
+
+        if correct and self.corrector is None:
+            raise ValueError('Correct set to true but no corrector available.')
+        
+        if correct:
+            et = self.corrector.apply_to(et, "et0")
+
+        return et
