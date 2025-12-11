5217bcbde29b139131881e20d28f1e5cd2602369
sitscholl <tscholl.simon@gmail.com>
2025-12-11 16:03:55 +0100

    add WaterBalanceWorkflow class that handles initialization and main workflow logic

--- main.py
+++ main.py
@@ -1,60 +1,32 @@
 #Example for base app classes structure: https://github.com/kthorp/pyfao56/blob/main/tests/test01/cottondry2013.py
 import logging
 import logging.config
-import sys
+from datetime import datetime
 
 from src.config import load_config
 from src.database.db import IrrigDB
-from src.field import FieldHandler
-from src.meteo import MeteoHandler
-from src.resample import MeteoResampler
-from src.et0.base import ET0Calculator
-from src.et_correction import ETCorrection
-from src.base_plot import BasePlot
+from src.workflow import WaterBalanceWorkflow
+
 
 logger = logging.getLogger(__name__)
 
-def main():
+if __name__ == "__main__":
     
     config = load_config('config/config.yaml')
     logging.config.dictConfig(config['logging'])
     db = IrrigDB(**config.get('database', {}))
 
-    if ET0Calculator.get_calculator_by_name(config['evapotranspiration']['method']) is None:
-        raise ValueError(f"ET0 calculator {config['evapotranspiration']['method']} not found. Choose one of {ET0Calculator.registry.keys()}")
+    logger.info("#"*50)
+    logger.info('Starting water balance calculation')
+    logger.info("#"*50)
 
-    fields = [FieldHandler(field) for field in db.get_all_fields()]
+    workflow = WaterBalanceWorkflow(config, db)
+    
+    try:
+        workflow.run()
+    except Exception as e:
+        logger.error(f"Error running water balance workflow: {e}", exc_info = True)
 
-    if len(fields) == 0:
-        logger.info('No fields configured in database. Terminating')
-        sys.exit(1)
-
-    meteo = MeteoHandler(config['meteo'])
-    resampler = MeteoResampler(**config['resampling'])
-    et_corrector = ETCorrection(**config['evapotranspiration']['correction'])
-    et_calculator = ET0Calculator.get_calculator_by_name(config['evapotranspiration']['method'])(corrector = et_corrector)
-
-    wb_plot = BasePlot().create_base(subpanels=1, vertical_spacing = .1, main_title="Microclimate & Irrigation")
-
-    reference_stations = set([i.reference_station for i in fields])
-    meteo.query(
-        provider = "SBR",
-        station_ids = reference_stations,
-        start = config['general']['season_start'],
-        end = config['general']['season_end'],
-        resampler = resampler
-    )
-    meteo.calculate_et(et_calculator, correct = True)
-
-    for field in fields:
-        field_irrigation_events = db.query_irrigation_event(field.name)
-        field.calculate_water_balance(
-            meteo.get_station_data(field.reference_station), field_irrigation_events
-            )
-
-        wb_plot.plot_field(field, field_irrigation_events)
-
-
-
-if __name__ == "__main__":
-    main()
+    logger.info("#"*50)
+    logger.info('Water balance calculation finished')
+    logger.info("#"*50)
---
+++ src/workflow.py
@@ -0,0 +1,83 @@
+import pandas as pd
+
+from dataclasses import dataclass
+import logging
+import sys
+from datetime import datetime
+
+from .field import FieldHandler
+from .meteo import MeteoHandler
+from .resample import MeteoResampler
+from .et0.base import ET0Calculator
+from .et_correction import ETCorrection
+from .base_plot import BasePlot
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class RuntimeContext:
+    meteo_handler: MeteoHandler
+    resampler: MeteoResampler
+    et_calculator: ET0Calculator
+
+class WaterBalanceWorkflow:
+
+    def __init__(self, config, db):
+        self.config = config
+        self.season_start = pd.to_datetime(config['general']['season_start'], dayfirst = True)
+        self.season_end = pd.to_datetime(config['general']['season_end'], dayfirst = True)
+        self.db = db
+
+        if ET0Calculator.get_calculator_by_name(config['evapotranspiration']['method']) is None:
+            raise ValueError(f"ET0 calculator {config['evapotranspiration']['method']} not found. Choose one of {ET0Calculator.registry.keys()}")
+
+        self.fields = [FieldHandler(field) for field in db.get_all_fields()]
+
+        if len(self.fields) == 0:
+            logger.info('No fields configured in database. Terminating')
+            sys.exit(1)
+
+        meteo = MeteoHandler(config['meteo'])
+        resampler = MeteoResampler(**config['resampling'])
+        et_corrector = ETCorrection(**config['evapotranspiration']['correction'])
+        et_calculator = ET0Calculator.get_calculator_by_name(config['evapotranspiration']['method'])(corrector = et_corrector)
+
+        self.runtime_context = RuntimeContext(
+            meteo_handler = meteo,
+            resampler = resampler,
+            et_calculator = et_calculator,
+        )
+
+        self.plot = BasePlot().create_base(subpanels=1, vertical_spacing = .1, main_title="Microclimate & Irrigation")
+
+        logger.info(f'Initialized WaterBalanceWorkflow with {len(self.fields)} fields from {self.season_start} to {self.season_end}.')
+
+    def run(self):
+
+        ## Query Meteo Data
+        reference_stations = set([i.reference_station for i in self.fields])
+        self.runtime_context.meteo.query(
+            provider = "SBR",
+            station_ids = reference_stations,
+            start = self.season_start,
+            end = min(datetime.today(), self.season_end),
+            resampler = self.runtime_context.resampler
+        )
+
+        ## Calculate evapotranspiration
+        self.runtime_context.meteo.calculate_et(self.runtime_context.et_calculator, correct = True)
+
+        ## Calculate water balance for each field
+        for field in self.fields:
+            try:
+                field_irrigation_events = self.db.query_irrigation_events(field.name)
+                field_wb = field.calculate_water_balance(
+                    self.runtime_context.meteo.get_station_data(field.reference_station), field_irrigation_events
+                    )
+                self.plot.plot_line(field_wb.index, field_wb["soil_storage"], name=field.name)
+                logger.debug(f"Calculated water-balance for field {field.name}")
+            except Exception as e:
+                logger.error(f"Error calculating water balance for field {field.name}: {e}")
+                continue
+
+            
