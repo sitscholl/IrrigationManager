24e9b7474567ad0bb3898d7b09327fb1815d6624
sitscholl <tscholl.simon@gmail.com>
2025-12-18 15:50:52 +0100

    add table below plot showing latest water balance per field

--- src/frontend/dashboard.py
+++ src/frontend/dashboard.py
@@ -1,12 +1,13 @@
 import asyncio
 import logging
+import pandas as pd
 
 from nicegui import ui
 
 from ..config import load_config
 from ..database.db import IrrigDB
 from ..workflow import WaterBalanceWorkflow
-
+from .deps import get_db
 from .header import add_header
 
 logger = logging.getLogger(__name__)
@@ -34,36 +35,55 @@ async def get_fig(force: bool = False):
             return _fig_cache
         _fig_cache = await asyncio.to_thread(build_waterbalance_fig)
         return _fig_cache
+        
+async def get_latest_water_balance(fields, db):
 
+    wb = []
+    for field in fields:
+        wb_field = db.latest_water_balance(field.id)
+        if wb_field is not None:
+            wb_field = pd.DataFrame(
+                columns = ['Anlage', 'Datum', 'Wasserbilanz'],
+                values =[wb_field.name, wb_field.date, wb_field.soil_storage]
+            )
+            wb.append(wb_field)
+
+    if len(wb) > 0:
+        return pd.concat(wb)
+    else:
+        logger.info("No water balance entries found")
+        return pd.DataFrame(columns = ['Anlage', 'Datum', 'Wasserbilanz'])
 
 @ui.page('/')
 async def dashboard():
     add_header()
-    
+    db = get_db()
+    fields = db.get_all_fields()
+    water_balance = await get_latest_water_balance(fields, db)
+
     with ui.column().classes("w-full max-w-4xl mx-auto gap-3 items-stretch"):
 
-        with ui.row().classes("w-full items-center justify-center"):
+        with ui.row().classes("w-full items-center"):
             ui.markdown('## Water balance')
             # ui.button(
             #     'Re-run workflow',
             #     on_click=lambda: asyncio.create_task(load_and_render(force=True)),
             # )
 
-        with ui.element().classes('flex-grow'):
-            container = ui.element().classes("w-full")
-            status = ui.label('Loading data...')
-            spinner = ui.spinner('bars', size='6em')
+        container = ui.element().classes("w-full")
 
-            async def load_and_render(force: bool = False):
-                fig = await get_fig(force=force)
-                # tighten figure height to reduce vertical whitespace
-                #fig.update_layout(height=500)
+        async def load_and_render(force: bool = False):
+            fig = await get_fig(force=force)
+            # tighten figure height to reduce vertical whitespace
+            # fig.update_layout(height=500)
 
-                status.delete()
-                spinner.delete()
-                container.clear()
-                # enter the slot explicitly to add new UI elements
-                with container:
-                    ui.plotly(fig).classes('w-full')
+            container.clear()
+            # enter the slot explicitly to add new UI elements
+            with container:
+                ui.plotly(fig).classes('w-full')
 
         asyncio.create_task(load_and_render())
+
+        ui.separator()
+
+        ui.table.from_pandas(water_balance)
